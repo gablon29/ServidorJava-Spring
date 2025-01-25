73bf034dd8ee7c269c1ab2b30931eadc4c777cc2
gabri <gabriellondero40@gmail.com>
2025-01-25 20:32:53 -0300

    terminando peticiones get.

--- src/main/java/com/example/prueba/TaskController.java
+++ src/main/java/com/example/prueba/TaskController.java
@@ -2,10 +2,9 @@ package com.example.prueba;
 
 import com.example.prueba.Model.Task;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/task")
@@ -21,5 +20,15 @@ public class TaskController {
     @PostMapping
     public Task craetedTask(@RequestBody Task task) {
         return taskServiceImp.saveTask(task);
+    }
+
+    @GetMapping()
+    public List<Task> getAllTask() {
+        return taskServiceImp.getAllTask();
+    }
+
+    @GetMapping("{id}")
+    public Task getTasById(@PathVariable Long id) {
+        return taskServiceImp.getById(id);
     }
 }
